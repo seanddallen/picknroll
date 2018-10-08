b18cef6e7514db7f6ec22c84684add8ce7c0f6f0
Sean Tayler <seanjtayler@gmail.com>
2018-10-08 11:31:04 -0700

    migration data

--- db/migrations/20181008104939_users.js
+++ db/migrations/20181008104939_users.js
@@ -1,8 +1,16 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('user_name');
+    table.string('email').unique();
+    table.string('password');
+    table.string('user_address');
+    table.string('user_city');
+    table.timestamps(true, true);
+  })
+
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users')
 };
