1efc4828f97f4b37a8d8e8346294ce9967d29c4c
Francis Francisco <francis.francisco44@gmail.com>
2018-10-09 14:28:08 -0700

    saved routes

--- config/routes.js
+++ config/routes.js
@@ -7,27 +7,22 @@ const votes = require("../controllers/votes.js");
 
 module.exports = function(app){
 
-  //LOGIN PAGE
   app.get('/', users.loginPage); //COMPLETE
   app.post('/login', users.login) //COMPLETE
   app.post('/register', users.create); //COMPLETE
 
   app.use(authMiddleware);
 
-  //COURTS PAGE
   app.get('/courts', courts.courtsPage); //shows all courts
   app.get('/courts/city/:city', courts.search);
-
   app.post('/courts/create', courts.createCourt); //COMPLETE
   app.post('/courts/vote/:id', votes.courtVote);
 
-  //COURT PAGE
   app.get('/courts/:id', courts.courtPage); //COMPLETE
-  app.get('/courts/description/:id', courts.description);
-  app.get('/courts/location/:id', courts.location);
-  app.get('/courts/images/:id', courts.images);
-  app.get('/courts/comments/:id', courts.comments);
-
+  // app.get('/courts/description/:id', courts.description);
+  // app.get('/courts/location/:id', courts.location);
+  // app.get('/courts/images/:id', courts.images);
+  // app.get('/courts/comments/:id', courts.comments);
   app.post('/courts/comments/newcomment', comments.newComment);
   app.post('/games/create', games.createGame);
 
